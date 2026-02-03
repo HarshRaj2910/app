0f836181ade0feb41c62380a2ad6661f66ac86b3
HarshRaj2910 <harshroman617@gmail.com>
2026-02-03 16:34:44 +0530

    f

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   const uri = process.env.MONGODB_URI;
+  const dbName = process.env.MONGODB_DB_NAME;
 
   if (!uri) {
     throw new Error('MONGODB_URI is not set in environment variables');
@@ -9,9 +10,12 @@ async function connectDB() {
 
   mongoose.set('strictQuery', true);
 
-  await mongoose.connect(uri, {
-    dbName: uri.split('/').pop(),
-  });
+  const options = {};
+  if (dbName) {
+    options.dbName = dbName;
+  }
+
+  await mongoose.connect(uri, options);
 
   // eslint-disable-next-line no-console
   console.log('MongoDB connected');
